26f9439be5bea9e4f9294ee3fe065dcf6680d965
Quetwoh <quetwoh@gmail.com>
2022-09-08 17:03:30 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,24 @@
+#!/bin/bash
+#incloud<stdio.h>
+
+
+
+/**
+
+* main-puts
+
+*
+
+*Return: is a (Successful)
+
+*/
+
+int main(Void)
+
+{
+
+   puts("\"programming is like building a multilimgual puzzle");
+
+   return (0);
+
+}
